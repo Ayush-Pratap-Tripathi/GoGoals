d7a4de9126c211316647780e4bfe78ca91dae864
Ayush Pratap <ayushprince110@gmail.com>
2026-04-23 00:41:31 +0530

    Rollback again

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,6 @@ app.use(express.urlencoded({ limit: '10mb', extended: true }));
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/goals', goalRoutes);
-app.use('/api/speech', speechRoutes);
 
 // Basic Route
 app.get('/', (req, res) => {
